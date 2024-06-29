12edb003925c38426c6424d7f49dfd649b6e4b9e
GuiTavs7 <7tavs7@gmail.com>
2024-06-29 13:41:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Acordeon
+
+## Descrição
+
+Projeto criado para fixação de conceitos aprendidos no módulo intermediário de JS do Dev Quest - FrontEnd!
+
+## Conceitos trabalhados e fixados no projeto:
+
+* Estrutura de pastas padrão no projeto; ✅
+* HTML Semântico; ✅
+* Display Flex; ✅
+* Responsividade; ✅
+* Aplicação de imagens de fundo; ✅
+* Validação de formulário via JS; ✅
+* Uso de funções; DOM e eventos no JS ✅
+
+## Design Desktop
+
+<div align="center">
+    <img src="./src/images/design/design_desktop.png" alt="desktop">
+</div>
+
+## Design Mobile
+
+<div align="center">
+    <img src="./src/images/design/design_mobile.png" alt="mobile">
+</div>
+
+## Campos Vazios
+
+Para qualquer campo vazio do formulário, a borda do input fica vermelha e uma mensagem "campo obrigatório" em vermelho aparece!
+
+<div align="center">
+    <img src="./src/images/design/campos_vazios.png" alt="mobile">
+</div>
+
+## Campos Preenchidos
+
+Para qualquer campo preenchido do formulário, a borda do input fica verde!
+
+<div align="center">
+    <img src="./src/images/design/campos_preenchidos.png" alt="mobile">
+</div>
+
+## Mensagem de alerta
+
+Quando todos campos estiverem preenchidos e se clicar no botão "enviar", uma mensagem de alerta com o nome do usuário aparece!
+
+<div align="center">
+    <img src="./src/images/design/alert_message.png" alt="mobile">
+</div>
---
+++ src/js/form.js
@@ -0,0 +1,45 @@
+const buttonSubmit = document.querySelector(".submit-button"); // Seleciona apenas uma classe
+const inputs = document.querySelectorAll(".input"); // Seleciona todos os elementos com a classe ".input"
+const spanMessages = document.querySelectorAll(".campo-obrigatorio"); // Seleciona todos os elementos com a classe ".campo-obrigatorio"
+
+// 1) CAMPOS VAZIOS DO FORM - VERMELHO
+
+buttonSubmit.addEventListener("click", () => {
+
+    const allFieldsFilled = Array.from(inputs).every((input) => input.value.trim() !== ""); // Só será true quando todos campos forem preenchidos
+
+    const name = inputs[0].value; // Armazena apenas o nome digitado pelo usuário no input de nome
+
+    // Se todos campos forem preenchidos:
+
+    if (allFieldsFilled) {
+        alert("Seu formulário foi enviado, " + name + "!");
+    }
+
+    inputs.forEach((input, index) => {
+        if (input.value === "") {
+            input.classList.add("campo-vazio");
+            spanMessages[index].classList.add("ativo");
+        } else {
+            input.classList.remove("campo-vazio");
+            spanMessages[index].classList.remove("ativo");
+        }
+    });
+});
+
+// 2) PREENCHIMENTO DO FORM - VERDE
+
+inputs.forEach(input => {
+
+    input.addEventListener("input", () => {
+
+        if(input.value !== ""){
+            input.classList.add("campo-preenchido");
+        }
+        else{
+            input.classList.remove("campo-preenchido");
+        }
+    
+    });
+
+});
